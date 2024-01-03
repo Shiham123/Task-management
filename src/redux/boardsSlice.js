2d68e833e40311b31d03d36cc9ddd99991dbe03b
Shiham123 <s36651786@gmail.com>
2024-01-03 16:57:36 +0600

    redux just added

---
+++ src/redux/boardsSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit';
+import data from '../../public/data.json';
+
+const boardsSlice = createSlice({
+  name: 'boards',
+  initialState: data.boards,
+  reducers: {
+    // TODO: write our reducers here
+  },
+});
