1f94c3ee9bcd85e83a20b0684606cc2937d8cf93
Shiham123 <s36651786@gmail.com>
2024-01-03 09:47:20 +0600

    drop down

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react';
+import { IoIosArrowDropdown, IoIosArrowDropup } from 'react-icons/io';
+
+import logoTwo from '../../src/assets/logoTwo.jpg';
+
 function Header() {
-  return <div>this is header</div>;
+  const [openDropDown, setOpenDropDown] = useState(false);
+  return (
+    <div className="p-4 fixed left-0 bg-white dark:bg-darkPrimary z-0 right-5">
+      <header className="flex justify-between dark:text-customWhite items-center">
+        {/* left side */}
+
+        <div className="flex items-center space-x-2 md:space-x-4">
+          <img src={logoTwo} alt="" className="h-6 w-6" />
+          <h3 className="hidden md:inline-block font-bold font-sans md:text-4xl">Shiham task management</h3>
+
+          <div className="truncate max-w-[200px] md:text-2xl text-xl font-bold md:ml-20 font-sans">
+            <h3>board Name</h3>
+          </div>
+          {openDropDown ? <IoIosArrowDropdown /> : <IoIosArrowDropup />}
+        </div>
+        {/* right side */}
+      </header>
+    </div>
+  );
 }
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default { content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'], darkMode: 'class', theme: { extend: {} }, plugins: [] };
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    colors: {
+      customWhite: '#ffffff',
+      darkPrimary: '#2b2c37',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
