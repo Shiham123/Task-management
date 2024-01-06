2c1fbcaf75514f3b16a4b5429f39506667897d43
Shiham123 <s36651786@gmail.com>
2024-01-06 23:03:26 +0600

    empty file style and also link to addBoardModalOpen

--- src/components/EmptyBoard/EmptyBoard.jsx
+++ src/components/EmptyBoard/EmptyBoard.jsx
@@ -1,8 +1,28 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import AddEditBoardModal from '../modals/AddEditBoardModal';
 
 function EmptyBoard(props) {
+  const [isBoardModalOpen, setIsBoardModalOpen] = useState(false);
+
   const { type } = props;
-  return <div>empty board {type}</div>;
+
+  return (
+    <div className="bg-white dark:bg-darkPrimary h-screen w-screen flex flex-col items-center justify-center">
+      <h3 className="text-gray-500 font-bold">
+        {type === 'edit' ? 'The board is empty. Create a new one.' : 'There are no boards available, create a new board to get started'}
+      </h3>
+      <button className="w-full items-center max-w-xs font-bold hover:opacity-70 dark:text-white dark:bg-customBgBtn mt-8 relative text-white bg-customBgBtn py-2 rounded-full">
+        {type === 'edit' ? '+ Add new Column' : '+ Add New Board'}
+      </button>
+
+      {isBoardModalOpen && (
+        <>
+          <AddEditBoardModal type={type} setBoardModalOpen={setIsBoardModalOpen} />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default EmptyBoard;
