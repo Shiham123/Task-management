c1711d2d0a6f75aa48ef3e365f52f4fb6222b6cf
Shiham123 <s36651786@gmail.com>
2024-01-06 18:52:02 +0600

    delete and cencel button created

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -8,7 +8,7 @@ function DeleteModal(props) {
       onClick={(e) => e.target === e.currentTarget && setIsDeleteModalOpen(false)}
       className="fixed right-0 left-0 top-0 bottom-0 px-2 py-4 overflow-scroll scrollbar-hide z-50 flex justify-center items-center bg-black/50"
     >
-      <div className="scrollbar-hide overflow-y-scroll max-h-[95vh] my-auto bg-white dark:bg-customCharade text-back dark:text-white w-full px-8 py-8 rounded-xl">
+      <div className="scrollbar-hide overflow-y-scroll max-w-md max-h-[95vh] my-auto bg-white dark:bg-customCharade text-back dark:text-white w-full px-8 py-8 rounded-xl">
         <h3 className="font-bold text-red-500 text-xl">Delete this {type} ? </h3>
         {type === 'task' ? (
           <p className="text-gray-500 font-semibold tracking-wider text-sm pt-6">
@@ -23,6 +23,13 @@ function DeleteModal(props) {
             This action cannot be reversed.
           </p>
         )}
+
+        <div className="flex w-full mt-4 items-center justify-center space-x-4">
+          <button className="w-full items-center text-white hover:opacity-75 font-semibold bg-red-500 py-2 rounded-full">Delete</button>
+          <button className="w-full items-center text-customBgBtn hover:opacity-75 font-semibold bg-customCharade/10 py-2 rounded-full">
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
   );
